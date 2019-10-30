77fad1a458951ee97ad7a34abb0cbea5391bd6cb
hchoi84@gmail.comlszYeuzF3EErZcYc6exit <hchoi84@gmail.com>
2019-10-29 20:31:44 -0700

    Basic setup to display page complete

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectTracker.Controllers
+{
+  public class HomeController : Controller
+  {
+    public IActionResult Index()
+    {
+      return View();
+    }
+  }
+}
